6d9b651791ceee26af9bc60fcae5427d0b393b99
amany2712 <am0932532@gmail.com>
2026-02-26 21:13:32 +0200

    add images

--- lib/app_images.dart
+++ lib/app_images.dart
@@ -10,12 +10,48 @@ class Assets {
   /// assets/images/Collection.png
   static const String imagesCollection = "assets/images/Collection.png";
 
+  /// Assets for imagesDress
+  /// assets/images/dress.png
+  static const String imagesDress = "assets/images/dress.png";
+
   /// Assets for imagesImage 20
   /// assets/images/image 20.png
   static const String imagesImage20 = "assets/images/image 20.png";
 
+  /// Assets for imagesListItem1
+  /// assets/images/list_item1.png
+  static const String imagesListItem1 = "assets/images/list_item1.png";
+
+  /// Assets for imagesListItem2
+  /// assets/images/list_item2.png
+  static const String imagesListItem2 = "assets/images/list_item2.png";
+
+  /// Assets for imagesListItem3
+  /// assets/images/list_item3.png
+  static const String imagesListItem3 = "assets/images/list_item3.png";
+
   /// Assets for imagesOctober
   /// assets/images/October.png
   static const String imagesOctober = "assets/images/October.png";
+
+  /// Assets for imagesPlaylet1
+  /// assets/images/playlet1.png
+  static const String imagesPlaylet1 = "assets/images/playlet1.png";
+
+  /// Assets for imagesPlaylet2
+  /// assets/images/playlet2.png
+  static const String imagesPlaylet2 = "assets/images/playlet2.png";
+
+  /// Assets for imagesRing
+  /// assets/images/ring.png
+  static const String imagesRing = "assets/images/ring.png";
+
+  /// Assets for imagesRings
+  /// assets/images/rings.png
+  static const String imagesRings = "assets/images/rings.png";
+
+  /// Assets for imagesShoe
+  /// assets/images/shoe.png
+  static const String imagesShoe = "assets/images/shoe.png";
 }
 
